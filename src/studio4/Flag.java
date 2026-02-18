0fc48a7136c0cf6130f7e5b12b63eb234f132be9
Kamakatcha1 <shafayasghar05@gmail.com>
2026-02-18 14:06:08 -0600

    Update Flag.java

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledRectangle(0, 0, 0.3333, 1);
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledRectangle(0.5, 0, 0.16666, 1);
+		StdDraw.setPenColor(0, 0, 255);
+		StdDraw.filledRectangle(1, 0, 0.3333, 1);
 	}
 }
